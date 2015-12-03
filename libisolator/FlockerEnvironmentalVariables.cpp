fddcdf72c696d7daf7462af234bbedbe58781682
Phil Winder <pnw@trifork.com>
2015-12-03 15:42:48 +0000

    Added FLOCKER_ID environmental var.

--- libisolator/FlockerEnvironmentalVariables.cpp
+++ libisolator/FlockerEnvironmentalVariables.cpp
@@ -23,12 +23,23 @@ Option<string> FlockerEnvironmentalVariables::getUserDir() {
     return this->userDir;
 }
 
+Option<string> FlockerEnvironmentalVariables::getUserFlockerId() {
+    return this->userFlockerId;
+}
+
 FlockerEnvironmentalVariables::FlockerEnvironmentalVariables(const ExecutorInfo &executorInfo) {
     foreach (const auto &variable, executorInfo.command().environment().variables()) {
         if (strings::startsWith(variable.name(), FlockerEnvironmentalVariables::FLOCKER_CONTAINER_VOLUME_PATH)) {
             this->userDir = variable.value();
             LOG(INFO) << "Container volume name ("
             << this->userDir.getOrElse("NO_DIR")
+            << ") parsed from environment";
+        } else if (strings::startsWith(variable.name(), FlockerEnvironmentalVariables::FLOCKER_ID)) {
+            this->userFlockerId = variable.value();
+            LOG(INFO) << "Using "
+            << FlockerEnvironmentalVariables::FLOCKER_ID
+            << " ("
+            << this->userFlockerId.getOrElse("NO ID")
             << ") parsed from environment";
         }
         // Add more parsers here.
@@ -38,5 +49,10 @@ FlockerEnvironmentalVariables::FlockerEnvironmentalVariables(const ExecutorInfo 
         LOG(ERROR) << "Could not parse" << FLOCKER_CONTAINER_VOLUME_PATH <<
         "from environmental variables. Not a Mesos-Flocker application";
         this->parseError = true;
+    }
+    if (this->getUserFlockerId().isNone()) {
+        LOG(WARNING)
+        << FlockerEnvironmentalVariables::FLOCKER_ID
+        << " not specified. You will not be able to migrate data on failover.";
     }
 }
